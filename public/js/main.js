a21549214115cb2de79da15587c54cd8434944b3
Vingt-six <devloppementmarouane@gmail.com>
2022-01-03 16:20:56 +0100

    projet fini sans finalisation 2 B.M

---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+import {sept,huit,neuf,plus,quatre,cinq,six,moins,un,deux,trois,fois,clear,zero,egale,divise, ecran } from "./module/create.js";
+
+// variable utilisé
+let number = [sept,huit,neuf,quatre,cinq,six,un,deux,trois,zero]
+let resultat;
+let ope;
+let nbr1;
+let oprateur = [plus,moins,fois,divise]
+
+// target les chiffres
+number.forEach(element => {
+    element.addEventListener('click', function(){
+        ecran.value += element.value
+    })
+});
+
+// target les opérteur
+oprateur.forEach(element => {
+    element.addEventListener('click', function(){
+        nbr1 = ecran.value
+        ecran.value = ''
+        ope = element.value
+    })
+});
+
+// Résultat
+egale.addEventListener('click', function(){
+    let nbr2 = parseInt(ecran.value)
+    switch (ope) {
+        case '+':
+            resultat = parseInt(nbr1) + nbr2
+            ecran.value = resultat
+            break;
+        case '-':
+            resultat = parseInt(nbr1) - nbr2
+            ecran.value = resultat
+            break;
+        case '*':
+            resultat = parseInt(nbr1) * nbr2  
+            ecran.value = resultat
+            break;
+        case '/':
+            resultat = parseInt(nbr1) / nbr2
+            ecran.value = resultat
+            break;
+    }
+})
+
+// "C" button clear
+clear.addEventListener('click', function(){
+    ecran.value = ''
+    nbr1 = ''
+    nbr2 = ''
+    ope = ''
+})
